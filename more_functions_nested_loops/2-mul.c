cf970d2cafb37594c5b77bd026b629a57c990e15
DavidPortillo26 <anicetod03@gmail.com>
2024-02-05 19:31:05 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,25 @@
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
+int mul(int a, int b)
+{
+	int result = 0;
+	int sign = (a < 0) ^ (b < 0) ? -1 : 1; /* Determine the sign of the result */
+
+	/* Take the absolute values for the multiplication */
+	a = (a < 0) ? -a : a;
+	b = (b < 0) ? -b : b;
+
+	/* Perform multiplication using addition */
+	while (b > 0)
+	{
+		result += a;
+		b--;
+	}
+
+	return sign * result;
+}
