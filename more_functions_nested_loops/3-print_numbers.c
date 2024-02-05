73e885d31f04ce338da1f642dc9a03a4cc9dddc0
DavidPortillo26 <anicetod03@gmail.com>
2024-02-05 20:51:28 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "print_numbers.h"
+#include "_putchar.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9 followed by a new line.
+ */
+void print_numbers(void)
+{
+char digit;
+
+for (digit = '0'; digit <= '9'; digit++)
+{
+_putchar(digit);
+if (digit != '9')
+_putchar(' ');
+}
+
+_putchar('\n');
+}
---
+++ more_functions_nested_loops/print_numbers.h
@@ -0,0 +1,6 @@
+#ifndef PRINT_NUMBERS_H
+#define PRINT_NUMBERS_H
+
+void print_numbers(void);
+
+#endif /* PRINT_NUMBERS_H */
