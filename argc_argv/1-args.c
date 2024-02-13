0a2af655652f33b248f59e72dcc7b8feff636f31
DavidPortillo26 <anicetod03@gmail.com>
2024-02-13 01:29:53 +0000

    1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -8,16 +8,9 @@
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
  */
-int main(int argc, char **argv)
+int main(int args, char *argv[])
 {
-	int i;
-
-	if (argc > 0)
-	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-	}
+	printf("%d\n", args - 1);
+	(void)argv;
 	return (0);
 }
