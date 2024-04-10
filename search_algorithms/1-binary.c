5eb251d29e5e286acc4e2b6087bf4e385c762922
DavidPortillo26 <anicetod03@gmail.com>
2024-04-10 21:25:02 +0000

    fixed 1-binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - search the first occurrence of a value in the array
+ * binary_search - search the first and last occurrence of a value in the array
  * @array: set of numbers
  * @size: size of the array
  * @value: value to search
@@ -9,8 +9,8 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t l = 0, r = size - 1;
-    int mid;
+    size_t l = 0, r = size - 1, mid;
+    int first_occurrence = -1;
 
     if (array == NULL)
         return (-1);
@@ -20,11 +20,33 @@ int binary_search(int *array, size_t size, int value)
         mid = l + (r - l) / 2;
 
         if (array[mid] == value)
-            return mid;
+        {
+            first_occurrence = mid;
+            r = mid - 1; /* Search for the last occurrence in the left half */
+        }
         else if (array[mid] < value)
             l = mid + 1;
         else
             r = mid - 1;
+    }
+
+    /* If the value was found, find the last occurrence */
+    if (first_occurrence != -1)
+    {
+        l = 0;
+        r = first_occurrence;
+        while (l <= r)
+        {
+            mid = l + (r - l) / 2;
+
+            if (array[mid] == value)
+                r = mid - 1; /* Search for the last occurrence in the left half */
+            else
+                l = mid + 1;
+        }
+
+        /* The last occurrence is at r + 1 */
+        return r + 1;
     }
 
     return (-1);
